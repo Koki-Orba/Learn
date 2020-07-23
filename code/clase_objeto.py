2c7cc40361190ffaf934eaedf8f4b45c99556192
Koki-Orba <coqui_ortega@hotmail.com>
2020-07-22 19:39:28 -0600

    Example 2 about class and objects

--- code/clase_objeto.py
+++ code/clase_objeto.py
@@ -1,7 +1,14 @@
-#Clase y objetos II
+#Class y objets II
 
-class persona:
-    doctor =  "victor"  #objeto
+class jugadores_a:
+#j1 y j2 are objets
+    j1 = "messi"
+    j2 = "c.ronaldo"
 
-#persona.doctor
-print(persona.doctor)
+#print(jugadores.j2)
+
+class jugadores_b:
+    j3 = "marcelo"
+    j1 = "falcao"
+
+print(jugadores_b.j1) #Falcao prints us because he only accesses the class data of jugadores_b
